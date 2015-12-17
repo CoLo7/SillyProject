2901122bbd987be5b1104d01a49b438d6922500f
CoLo7 <gabeaguilar7@gmail.com>
2015-12-17 08:38:10 -0700

    Added black button, green switch, red button, and random color change button.

--- SillyProject/ViewController.swift
+++ SillyProject/ViewController.swift
@@ -8,14 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    
+    @IBAction func randomChange(sender: UIButton)
+    {
+        randomColorChange()
+    }
+    @IBAction func redChange(sender: UIButton)
+    {
+        view.backgroundColor = UIColor.redColor()
+    }
+    @IBAction func greenChange(sender: UISwitch)
+    {
+        view.backgroundColor = UIColor.greenColor()
+    }
 
-    override func viewDidLoad() {
+    @IBAction func changeColor(sender: UIButton)
+    {
+        view.backgroundColor = UIColor.blackColor()
+        
+    }
+    
+    func randomColorChange()
+    {
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
